3b6358c2e04408cff052384dfadd20dd006894e3
thanhthu1203 <ITCSIU24083@student.hcmiu.edu.vn>
2025-11-21 10:50:38 +0700

    Add files via upload

---
+++ backend/src/main/java/com/example/onlineexam/model/Course.java
@@ -0,0 +1,46 @@
+package com.example.onlineexam.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Course")
+public class Course {
+
+    @Id
+    @Column(name = "Course_ID", length = 7)
+    private String courseId;
+
+    @Column(name = "CName")
+    private String cName;
+
+    @OneToMany(mappedBy = "course")
+    private Set<Exam> exams = new HashSet<>();
+
+    // Getters and setters
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getcName() {
+        return cName;
+    }
+
+    public void setcName(String cName) {
+        this.cName = cName;
+    }
+
+    public Set<Exam> getExams() {
+        return exams;
+    }
+
+    public void setExams(Set<Exam> exams) {
+        this.exams = exams;
+    }
+}
